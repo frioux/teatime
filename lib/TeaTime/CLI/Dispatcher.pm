cdceebf04332d7222e4c4f512cfd06f850a83ec3
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-06-29 16:40:50 -0500

    super simple working dispatcher

--- lib/TeaTime/CLI/Dispatcher.pm
+++ lib/TeaTime/CLI/Dispatcher.pm
@@ -2,42 +2,56 @@ package TeaTime::CLI::Dispatcher;
 
 use 5.12.1;
 
-use Path::Dispatcher::Declarative -base;
 use TeaTime::Schema;
 
-chdir '~';
 my $schema = TeaTime::Schema->connect('dbi:SQLite:dbname=.teadb');
 my $tea_rs = $schema->resultset('Tea');
 my $tea_time_rs = $schema->resultset('TeaTime');
 
-on init => sub { $schema->deploy unless -e '.teadb' };
-
-on [set => qr/^\w+$/] => sub {
-   $tea_time_rs->create({ tea => { name => $2 } })
-};
-
-on [create => qr/^\w+$/] => sub { $tea_rs->create({ name => $2, enabled => 1 }) };
-
-under list => sub {
-   on 'teas' => sub {
-      say $_ for $tea_rs->search({ enabled => 1 }, { order_by => 'name' })
-         ->get_column('name')->all
-   };
-
-   on 'times' => sub {
-      say $_->when_occured->ymd . ': ' . $_->tea->name
-         for $tea_time_rs->search(undef, {
-            prefetch => 'tea',
-            order_by => 'when_occured'
-         })->all
-   };
-};
-on [update => '*', '*'] => sub {
-   $tea_rs->search({ name => $2 })->update({ name => $3 })
-};
-
-on [toggle => qr/^\w+$/] => sub {
-   $tea_rs->single({ name => $2 })->toggle->update
-};
+sub dispatch {
+   my $args = $_[1];
+   given ($args->[0]) {
+      when ('init') {
+         $schema->deploy unless -e '.teadb'
+      }
+      when ('set') {
+         $tea_time_rs->create({ tea => { name => $args->[1] } })
+      }
+      when ('clear') {
+         $tea_rs->delete; $tea_time_rs->delete
+      }
+      when ('create') {
+         $tea_rs->create({ name => $args->[1], enabled => 1 })
+      }
+      when ('list') {
+         given ($args->[1]) {
+            when ('teas') {
+               say $_ for $tea_rs->search({ enabled => 1 }, { order_by => 'name' })
+                  ->get_column('name')->all
+            }
+            when ('times') {
+               say sprintf '%s: %s', $_->when_occured->ymd, $_->tea->name
+                  for $tea_time_rs->search(undef, {
+                     prefetch => 'tea',
+                     order_by => 'when_occured'
+                  })->all
+            }
+         }
+      }
+      when ('toggle') {
+         $tea_rs->single({ name => $args->[1] })->toggle->update
+      }
+      when ('server') {
+         require Plack::Runner;
+         my $runner = Plack::Runner->new(
+            server => 'Starman', env => 'deployment'
+         );
+         my @args = ('lib/TeaTime/Web.pm', '-D', '-p', 8320, '-o', 'valium.lan.mitsi.com');
+         $runner->parse_options(@args);
+         $runner->set_options(argv => \@args);
+         $runner->run;
+      }
+   }
+}
 
 1;
