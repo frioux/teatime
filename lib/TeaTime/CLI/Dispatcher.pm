b62b192f4d749c23b14388a9d4c3781f62381ff5
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-10-16 13:34:54 -0500

    nicer ui for setting teas

--- lib/TeaTime/CLI/Dispatcher.pm
+++ lib/TeaTime/CLI/Dispatcher.pm
@@ -8,6 +8,24 @@ my $schema = TeaTime::Schema->connect('dbi:SQLite:dbname=.teadb');
 my $tea_rs = $schema->resultset('Tea');
 my $tea_time_rs = $schema->resultset('TeaTime');
 
+sub single_tea {
+   my $action = $_[0];
+   my $name   = $_[1];
+   my $teas = $tea_rs->cli_find($name);
+   my $count = $teas->count;
+   if ($count > 1) {
+      say 'More than one tea found:';
+      my $x = 0;
+      print join '', map ++$x . '. ' . $_->name . "\n", $teas->all;
+      exit 1;
+   } elsif ($count == 1) {
+      $action->($teas->single)
+   } else {
+      say "No tea with with name «$name» found";
+      exit 1;
+   }
+}
+
 sub dispatch {
    my $args = $_[1];
    given ($args->[0]) {
@@ -15,7 +33,10 @@ sub dispatch {
          $schema->deploy
       }
       when ('set') {
-         $tea_time_rs->create({ tea => { name => $args->[1] } })
+         single_tea(sub {
+            say 'Setting tea to ' . $_[0]->name;
+            $tea_time_rs->create({ tea_id => $_[0]->id })
+         }, $args->[1]);
       }
       when ('clear') {
          $tea_rs->delete; $tea_time_rs->delete
@@ -46,7 +67,10 @@ sub dispatch {
          }
       }
       when ('toggle') {
-         $tea_rs->single({ name => $args->[1] })->toggle->update
+         single_tea(sub {
+            say 'Toggling ' . $_[0]->name . ' to ' . (!$_[0]->enabled?'disabled':'enabled');
+            $_[0]->toggle->update;
+         }, $args->[1]);
       }
       when ('server') {
          require Plack::Runner;
@@ -57,6 +81,11 @@ sub dispatch {
          $runner->parse_options(@args);
          $runner->set_options(argv => \@args);
          $runner->run;
+      }
+      default {
+         print <<'USAGE';
+ perl teatime init|set $tea|clear|create $tea|list|toggle $tea|server
+USAGE
       }
    }
 }
--- lib/TeaTime/Schema/ResultSet/Tea.pm
+++ lib/TeaTime/Schema/ResultSet/Tea.pm
@@ -2,4 +2,6 @@ package TeaTime::Schema::ResultSet::Tea;
 
 use parent 'DBIx::Class::ResultSet';
 
+sub cli_find { $_[0]->search({ name => { -like => "%$_[1]%" } }) }
+
 1;
