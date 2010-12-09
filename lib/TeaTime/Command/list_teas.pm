cb033c6e8e5351a78644497599c541a692eae990
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-08 23:28:08 -0600

    add list_teas command

---
+++ lib/TeaTime/Command/list_teas.pm
@@ -0,0 +1,18 @@
+package TeaTime::Command::list_teas;
+
+use TeaTime -command;
+
+use 5.12.1;
+use warnings;
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  my $x = 0;
+  print join '', map sprintf("%2d. %s %s\n", ++$x, ($_->enabled?'*':' '), $_->name),
+    $self->app->tea_rs->search(undef, {
+      order_by => [{ -desc => 'enabled' }, 'name' ]
+    })->all
+}
+
+1;
