e2fbe7fa3ba64a2dbe9df3f4d8e41df1e5fa2aa9
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-08 23:31:40 -0600

    add list_contacts command

---
+++ lib/TeaTime/Command/list_contacts.pm
@@ -0,0 +1,15 @@
+package TeaTime::Command::list_contacts;
+
+use TeaTime -command;
+
+use 5.12.1;
+use warnings;
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  my $x = 0;
+  print map ++$x . '. ' . $_ . "\n", $self->app->contact_rs->get_column('jid')->all
+}
+
+1;
