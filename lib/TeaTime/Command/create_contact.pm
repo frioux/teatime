d27eb5e051321156a74b147a0fd348f4857871da
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-09 00:04:23 -0600

    validate contact

--- lib/TeaTime/Command/create_contact.pm
+++ lib/TeaTime/Command/create_contact.pm
@@ -7,6 +7,14 @@ use warnings;
 
 sub abstract { 'create a new contact' }
 
+sub usage_desc { 't create_contact <contact>' }
+
+sub validate_args {
+  my ($self, $opt, $args) = @_;
+
+  $self->usage_error('you forgot to pass a new contact!') unless @$args;
+}
+
 sub execute {
   my ($self, $opt, $args) = @_;
 
