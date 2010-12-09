657d0d22ff427d998d2599c5934cce89681fbfae
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-08 23:29:47 -0600

    add list_times command

---
+++ lib/TeaTime/Command/list_times.pm
@@ -0,0 +1,19 @@
+package TeaTime::Command::list_times;
+
+use TeaTime -command;
+
+use 5.12.1;
+use warnings;
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  my $x = 0;
+  say sprintf '%s: %s', $_->when_occured->ymd, $_->tea->name
+     for $self->app->tea_time_rs->search(undef, {
+        prefetch => 'tea',
+        order_by => 'when_occured'
+     })->all
+}
+
+1;
