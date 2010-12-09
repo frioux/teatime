6e9059b56d2194f971096a6898f7c5dcf2c9f2e0
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-08 23:15:13 -0600

    undo command

---
+++ lib/TeaTime/Command/undo.pm
@@ -0,0 +1,22 @@
+package TeaTime::Command::undo;
+
+use TeaTime -command;
+
+use 5.12.1;
+use warnings;
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  my $t = $self->app->tea_time_rs->search(undef, {
+    order_by => { -desc => 'when_occured' }
+  })->first;
+  if ($args->[0]) {
+    say 'undoing last tea time: ' . $t->when_occured->ymd . ', ' . $t->tea->name;
+    $t->delete;
+  } else {
+    say 'would undo last tea time: ' . $t->when_occured->ymd . ', ' . $t->tea->name;
+  }
+}
+
+1;
