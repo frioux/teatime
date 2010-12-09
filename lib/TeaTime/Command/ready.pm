802d0aa2dec7cf13f11d94d7fc79b8a74f161d29
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-08 23:25:53 -0600

    add ready command

---
+++ lib/TeaTime/Command/ready.pm
@@ -0,0 +1,20 @@
+package TeaTime::Command::ready;
+
+use TeaTime -command;
+
+use 5.12.1;
+use warnings;
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  my $ttrs = $self->app->tea_time_rs;
+
+  $ttrs->in_order->first->events->create({
+    type => { name => 'Ready' }
+  });
+  $self->app->send_message('Tea ready: ' . $ttrs->first->tea->name .
+    ' (http://valium.lan.mitsi.com:8320) (http://akama.lan.mitsi.com:5000)');
+}
+
+1;
