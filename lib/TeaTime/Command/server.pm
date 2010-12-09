1398130f1cd99b3881aad0fe30214c40c243e30a
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-08 23:41:21 -0600

    add server command

---
+++ lib/TeaTime/Command/server.pm
@@ -0,0 +1,28 @@
+package TeaTime::Command::server;
+
+use TeaTime -command;
+
+use FindBin;
+use 5.12.1;
+use warnings;
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  require Plack::Runner;
+  my $runner = Plack::Runner->new(
+     server => 'Starman', env => 'deployment'
+  );
+  my @args = (
+    "$FindBin::Bin/../lib/TeaTime/Web.pm",
+    '-I', "$FindBin::Bin/../lib",
+    '-D',
+    '-p', 8320,
+    '-o', 'valium.lan.mitsi.com'
+  );
+  $runner->parse_options(@args);
+  $runner->set_options(argv => \@args);
+  $runner->run;
+}
+
+1;
