a148bf9b20b891da09f32ffd494dd1669a3a028e
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-08 23:41:13 -0600

    add toggle_contact command

---
+++ lib/TeaTime/Command/toggle_contact.pm
@@ -0,0 +1,17 @@
+package TeaTime::Command::toggle_contact;
+
+use TeaTime -command;
+
+use 5.12.1;
+use warnings;
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  $self->app->single_item(sub {
+     say 'Toggling ' . $_[0]->jid . ' to ' . ($_[0]->enabled?'disabled':'enabled');
+     $_[0]->toggle->update;
+  }, 'contact', $args->[0], $self->app->contact_rs);
+}
+
+1;
