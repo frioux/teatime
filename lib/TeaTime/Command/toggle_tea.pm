4317bf0a1342883097d3406d37064145fb7241f2
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-08 23:34:43 -0600

    add toggle_tea command

---
+++ lib/TeaTime/Command/toggle_tea.pm
@@ -0,0 +1,17 @@
+package TeaTime::Command::toggle_tea;
+
+use TeaTime -command;
+
+use 5.12.1;
+use warnings;
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  $self->app->single_item(sub {
+     say 'Toggling ' . $_[0]->name . ' to ' . ($_[0]->enabled?'disabled':'enabled');
+     $_[0]->toggle->update;
+  }, 'tea', $args->[0], $self->app->tea_rs);
+}
+
+1;
