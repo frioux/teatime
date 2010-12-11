af8668af83ee672ff535838c998e88a0441e4297
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-11 01:55:02 -0600

    much nicer timer

--- lib/TeaTime/Command/timer.pm
+++ lib/TeaTime/Command/timer.pm
@@ -19,6 +19,7 @@ sub execute {
      type => { name => 'Started Steep' }
   });
 
+  $|++; # print immediately, not after a newline
   require AnyEvent;
   my $j = AnyEvent->condvar;
   my $once;
@@ -26,10 +27,11 @@ sub execute {
   my $w; $w = AnyEvent->timer (
      interval => 1,
      cb       => sub {
-        say $seconds--;
+        print "\r\x1b[J" . $seconds--;
         if ($seconds < 0 && !$once) {
            use Term::ReadKey;
            ReadMode 4; # Turn off controls keys
+           print "\a"; # BEEP
            1 while !defined ReadKey(-1);
            ReadMode 0; # Reset tty mode before exiting
            $tea_time_rs->in_order->first->events->create({
