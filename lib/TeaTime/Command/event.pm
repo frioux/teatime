48fa7e75fc648af702ead78750ef18bffcecf1da
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-12-23 04:47:55 -0600

    add event command

---
+++ lib/TeaTime/Command/event.pm
@@ -0,0 +1,29 @@
+package TeaTime::Command::event;
+
+use TeaTime -command;
+
+use 5.12.1;
+use warnings;
+
+sub abstract { 'mark pot with arbitrary event' }
+
+sub usage_desc { 't event <event-name>' }
+
+sub validate_args {
+  my ($self, $opt, $args) = @_;
+
+  $self->usage_error('too few arguments') unless scalar @$args == 1;
+}
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+   $self->app->tea_time_rs->in_order->first->events->create({
+      type => { name => $args->[0] }
+   });
+
+   say "Adding event «$args->[0]» to pot";
+}
+
+1;
+
