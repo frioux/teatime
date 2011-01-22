52ce6c8be11184061c50fce3c50eacd2dd34f3a4
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2011-01-22 15:15:53 -0600

    initial cut at an api

---
+++ lib/TeaTime/Web/API.pm
@@ -0,0 +1,57 @@
+package TeaTime::Web::API;
+
+use Moo;
+use HTTP::Tiny;
+use URI;
+use Sub::Quote;
+use JSON;
+
+has http_tiny => (
+   is => 'ro',
+   default => quote_sub q{ HTTP::Tiny->new( timeout => 1 ) },
+);
+
+has api_root => (
+   is => 'ro',
+   default => quote_sub q{ 'http://localhost:5000' },
+);
+
+
+sub _dejson { decode_json($_[0]->{content})->{data} }
+
+sub _build_url {
+   my ($self, $url, $params) = @_;
+
+   my $uri = URI->new($self->api_root . $url);
+
+   $uri->query_form($params);
+
+   "$uri"
+}
+
+sub _post   { $_[0]->http_tiny->request( POST   => $_[0]->_build_url($_[1], $_[2]) ) }
+sub _get    { $_[0]->http_tiny->request( GET    => $_[0]->_build_url($_[1], $_[2]) ) }
+sub _put    { $_[0]->http_tiny->request( PUT    => $_[0]->_build_url($_[1], $_[2]) ) }
+sub _delete { $_[0]->http_tiny->request( DELETE => $_[0]->_build_url($_[1], $_[2]) ) }
+
+sub add_contact { $_[0]->_post('/contacts', { jid => $_[1] }) }
+sub get_contacts { $_[0]->_get('/contacts') }
+
+sub add_event { $_[0]->_post('/events', { name => $_[1] }) }
+
+sub add_milk { $_[0]->_post('/milk', { date => $_[1] }) }
+sub get_milk { $_[0]->_get('/milk') }
+
+sub add_tea {
+   $_[0]->_post('/teas', {
+      name => $_[1],
+      steep_time => $_[2],
+      is_heaping => $_[3]
+   })
+}
+sub get_tea { _dejson($_[0]->_get('/teas')) }
+
+sub add_tea_time { $_[0]->_post('/teatime', { name => $_[1] }) }
+sub get_tea_times { $_[0]->_get('/teatime') }
+
+1;
