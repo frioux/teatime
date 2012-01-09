a339f364bf19bd0be092b83d5104a82441f1e40b
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-01-09 16:17:17 -0600

    allow sending messages without quotes

--- lib/TeaTime/Command/message.pm
+++ lib/TeaTime/Command/message.pm
@@ -12,10 +12,10 @@ sub usage_desc { 't message <message>' }
 sub validate_args {
   my ($self, $opt, $args) = @_;
 
-  $self->usage_error('too few arguments') unless scalar @$args == 1;
+  $self->usage_error('too few arguments') unless scalar @$args >= 1;
 }
 
-sub execute { $_[0]->app->send_message($_[2]->[0]) }
+sub execute { $_[0]->app->send_message(join q( ), @{$_[2]}) }
 
 1;
 
