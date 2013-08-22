28ad2f37e33f378bb9e642dea2e4be91b2241018
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2013-08-22 13:56:20 -0500

    allow only showing enabled teas

--- lib/TeaTime/Command/list_teas.pm
+++ lib/TeaTime/Command/list_teas.pm
@@ -7,14 +7,22 @@ use warnings;
 
 sub abstract { 'print a list of teas' }
 
+sub opt_spec {
+  return (
+    [ "on-hand|O",  "only show teas on hand" ],
+  );
+}
+
 sub execute {
   my ($self, $opt, $args) = @_;
 
   my $x = 0;
+  my $rs = $self->app->tea_rs->search(undef, {
+    order_by => [{ -desc => 'enabled' }, 'name' ]
+  });
+  $rs = $rs->enabled if $opt->{on_hand};
   print join '', map sprintf("%2d. %s %s\n", ++$x, ($_->enabled?'*':' '), $_->name),
-    $self->app->tea_rs->search(undef, {
-      order_by => [{ -desc => 'enabled' }, 'name' ]
-    })->all
+    $rs->all
 }
 
 1;
