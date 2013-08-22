816ccb233dc3732672cc3b87286dddadc3c02b32
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2013-08-22 14:51:47 -0500

    add order-by-drank option

--- lib/TeaTime/Command/list_teas.pm
+++ lib/TeaTime/Command/list_teas.pm
@@ -10,6 +10,7 @@ sub abstract { 'print a list of teas' }
 sub opt_spec {
   return (
     [ "on-hand|O",  "only show teas on hand" ],
+    [ "order-by-drank|D",  "order_by last drank" ],
   );
 }
 
@@ -17,9 +18,14 @@ sub execute {
   my ($self, $opt, $args) = @_;
 
   my $x = 0;
-  my $rs = $self->app->tea_rs->search(undef, {
-    order_by => [{ -desc => 'enabled' }, 'name' ]
-  });
+  my $rs = $self->app->tea_rs;
+  if ($opt->order_by_drank) {
+    $rs = $rs->order_by_last_drank
+  } else {
+    $rs = $rs->search(undef, {
+       order_by => [{ -desc => 'enabled' }, 'name' ]
+     })
+  }
   $rs = $rs->enabled if $opt->{on_hand};
   print join '', map sprintf("%2d. %s %s\n", ++$x, ($_->enabled?'*':' '), $_->name),
     $rs->all
--- lib/TeaTime/Schema/ResultSet/Tea.pm
+++ lib/TeaTime/Schema/ResultSet/Tea.pm
@@ -3,10 +3,23 @@ package TeaTime::Schema::ResultSet::Tea;
 use parent 'TeaTime::Schema::ResultSet';
 __PACKAGE__->load_components(qw(
    Helper::ResultSet::Random
+   Helper::ResultSet::CorrelateRelationship
 ));
 
 sub cli_find { $_[0]->search({ name => { -like => "%$_[1]%" } }) }
 
 sub enabled { $_[0]->search({ enabled => 1}) }
+
+sub order_by_last_drank {
+   $_[0]->search(undef, {
+      order_by => {
+         -desc => $_[0]->correlate('tea_times')
+            ->search(undef, { rows => 1 })
+            ->in_order
+            ->get_column('when_occured')
+            ->as_query
+      },
+   });
+}
 
 1;
