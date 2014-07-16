d743853bafade3b3437e5a6b6063020eb300c35d
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-07-16 14:06:43 -0500

    woo colored lights!

--- lib/TeaTime/Command/timer.pm
+++ lib/TeaTime/Command/timer.pm
@@ -15,44 +15,51 @@ sub execute {
 
   my $seconds = $args->[0] || $tea_time->tea->steep_time;
 
-  $tea_time->events->create({
-     type => { name => 'Started Steep' }
-  });
+  $self->app->schema->txn_do(sub {
+     $tea_time->events->create({
+        type => { name => 'Started Steep' }
+     });
 
-  $|++; # print immediately, not after a newline
-  require AnyEvent;
-  my $j = AnyEvent->condvar;
-  my $once = 0;
-  my $w = AnyEvent->timer (
-     interval => 1,
-     cb       => sub {
-        if ($seconds >= 0) {
-           print "\r\x1b[J" . $seconds--;
-        } elsif (!$once) {
-           use Term::ReadKey;
-           ReadMode 4; # Turn off controls keys
-           print "\a"; # BEEP
-           1 while !defined ReadKey(-1);
-           ReadMode 0; # Reset tty mode before exiting
-           $tea_time->events->create({
-              type => { name => 'Stopped Steep' }
-           });
-           $tea_time->events->create({
-             type => { name => 'Ready' }
-           });
-           $self->app->send_message(
-             (sprintf 'Tea ready: %s (%s) (%s)',
-               $tea_time->tea->name,
-               $self->app->config->{servers}{api},
-               $self->app->config->{servers}{human}
-             ),
-             $j
-           );
-           $once = 1
+     $|++; # print immediately, not after a newline
+     require AnyEvent;
+     my $j = AnyEvent->condvar;
+     my $once = 0;
+     my $start_seconds = $seconds;
+     my $w = AnyEvent->timer (
+        interval => 1,
+        cb       => sub {
+           if ($seconds >= 0) {
+              print "\r\x1b[J" . $seconds--;
+              my $color = (255 * (($start_seconds - $seconds)/$start_seconds)) . ',0,0';
+              system(qw(/home/frew/Sync/bin/blink1-tool --rgb), $color);
+           } elsif (!$once) {
+              use Term::ReadKey;
+              ReadMode 4; # Turn off controls keys
+              print "\a"; # BEEP
+              system(qw(/home/frew/Sync/bin/blink1-tool --blue));
+              1 while !defined ReadKey(-1);
+              system(qw(/home/frew/Sync/bin/blink1-tool --off));
+              ReadMode 0; # Reset tty mode before exiting
+              $tea_time->events->create({
+                 type => { name => 'Stopped Steep' }
+              });
+              $tea_time->events->create({
+                type => { name => 'Ready' }
+              });
+              $self->app->send_message(
+                (sprintf 'Tea ready: %s (%s) (%s)',
+                  $tea_time->tea->name,
+                  $self->app->config->{servers}{api},
+                  $self->app->config->{servers}{human}
+                ),
+                $j
+              );
+              $once = 1
+           }
         }
-     }
-  );
-  $j->recv;
+     );
+     $j->recv;
+  })
 }
 
 1;
