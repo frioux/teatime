5718504d368b77dff76eb06fdb6891b6b42bd7c0
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-07-16 14:10:09 -0500

    correlate_last_drank

--- lib/TeaTime/Schema/ResultSet/Tea.pm
+++ lib/TeaTime/Schema/ResultSet/Tea.pm
@@ -12,14 +12,24 @@ sub enabled { $_[0]->search({ enabled => 1}) }
 
 sub order_by_last_drank {
    $_[0]->search(undef, {
-      order_by => {
-         -desc => $_[0]->correlate('tea_times')
-            ->search(undef, { rows => 1 })
-            ->in_order
-            ->get_column('when_occured')
-            ->as_query
-      },
+      order_by => { -desc => $_[0]->correlated_last_drank },
    });
+}
+
+sub with_last_drank {
+   $_[0]->search(undef, {
+      '+columns' => {
+         last_drank => $_[0]->correlated_last_drank,
+      },
+   })
+}
+
+sub correlated_last_drank {
+   $_[0]->correlate('tea_times')
+      ->search(undef, { rows => 1 })
+      ->in_order
+      ->get_column('when_occured')
+      ->as_query
 }
 
 1;
